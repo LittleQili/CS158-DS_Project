a6081b49f716eb603376b44bd37d33c7012b1f21
Yijia_Diao <diao_yijia@sjtu.edu.cn>
2019-06-06 14:01:20 +0800

    almost finish IO,finish con and destruct,modifiy data members

--- BTree/BTree.hpp
+++ BTree/BTree.hpp
@@ -7,33 +7,32 @@
 #include <cstring>
 
 namespace sjtu {
-    typedef long PTR_FILE_CONT;//说实话这个我真的不知道应该设置为哪种数据类型。
+    typedef long PTR_FILE_CONT;///说实话这个我真的不知道应该设置为哪种数据类型。
     const int IOnum = 4096;
     //目前选择写在一个文件里面吧，这样的IO次数可能也不多。
-    const char EMPTY_NAME_FILE = '\0';
-    const size_t CHAR_SIZE = sizeof(char);
+    char EMPTY_NAME_FILE[10] = "my.dat";///还有这个，，文件命名应该怎么命名啊
     const size_t PTR_FILE_SIZE = sizeof(PTR_FILE_CONT);
-    const size_t MAX_NUM_STACK = 100;
+    const size_t MAX_NUM_STACK = 10;
 
-#define IS_STRUCTURING_BPT
+//#define DEBUGGING
     template <class Key, class Value, class Compare = std::less<Key> >
     class BTree {
     private:
         struct data_index;
         struct data_leaf;
-#ifdef IS_STRUCTURING_BPT
-    public:
+#ifdef DEBUGGING
+        public:
 #else
-        private:
+    private:
 #endif
-        static const size_t MAX_NUM_INDEX = (IOnum - 4 * PTR_FILE_SIZE - sizeof(short)
+        static const size_t MAX_NUM_INDEX = (IOnum - 3 * PTR_FILE_SIZE - sizeof(short)
                                              - sizeof(char)) / sizeof(data_index);
         static const size_t MIN_NUM_INDEX = MAX_NUM_INDEX>>1;
         static const size_t MAX_NUM_LEAF = (IOnum - 3 * PTR_FILE_SIZE - sizeof(short)
                                             - sizeof(char)) / sizeof(data_leaf);
         static const size_t MIN_NUM_LEAF = MAX_NUM_LEAF>>1;
-        // Your private members go here
     private:
+        ///所有的拷贝构造都没写，蛤。
         struct data_index{
             PTR_FILE_CONT prev;
             Key keydata;
@@ -43,37 +42,39 @@ namespace sjtu {
             Value valdata;
         };
         struct Node_index{
-            const char type = 'i';
+            const char type;
             PTR_FILE_CONT myself;//不知道这个到底有没有必要？？？
             PTR_FILE_CONT next,prev;
-            PTR_FILE_CONT maxchild;
-            short curnum = 0;
+            short curnum;
             data_index index[MAX_NUM_INDEX];
+            Node_index():type('i'),curnum(0),myself(-1),next(-1),prev(-1){};
         };
         struct Node_leaf{
-            const char type = 'l';
+            const char type;
             PTR_FILE_CONT myself;//不知道这个到底有没有必要？？？
             PTR_FILE_CONT next,prev;
-            short curnum = 0;
+            short curnum;
             data_leaf leaf[MAX_NUM_LEAF];
+            Node_leaf():type('l'),curnum(0),myself(-1),next(-1),prev(-1){};
         };
         //root:最初始的值就是紧接着BPT之后的一个4k块，之后的话就不好说了，可以随便指向哪里
         //endoffset_file:文件的最后一个byte的offset,文件空的时候为0.
         //firstleaf：最初始不就是root嘛？
-        //等等，这里的初始值似乎有点问题。应该等第一个块插进来之后再去modify root和firstleaf的值。
+        ///等等，这里的初始值似乎有点问题。应该等第一个块插进来之后再去modify root和firstleaf的值。
         struct BPT{
-            PTR_FILE_CONT root = sizeof(BPT);
-            PTR_FILE_CONT firstleaf = sizeof(BPT);
-            PTR_FILE_CONT endoffset_file = 0;
-            long sumnum_data = 0;
-            int height = 0;
+            PTR_FILE_CONT root;
+            PTR_FILE_CONT firstleaf;
+            PTR_FILE_CONT endoffset_file;
+            long sumnum_data;
+            int height;//这里height是包含叶子那一层的。
+            BPT():root(-1),firstleaf(-1),endoffset_file(0),sumnum_data(0),height(0){};
         };
 
         class myStack{
         private:
             Node_index **index;
-            size_t curnum = 0;
-            size_t curmax = MAX_NUM_STACK;
+            size_t curnum;
+            size_t curmax;
             void doublespace(){
                 Node_index** del = index;
                 index = new Node_index*[curmax * 2];
@@ -85,7 +86,7 @@ namespace sjtu {
                 delete []del;
             }
         public:
-            myStack():curmax(MAX_NUM_STACK){
+            myStack():curnum(0),curmax(MAX_NUM_STACK){
                 index = new Node_index*[curmax];
                 for(int i = 0;i < curmax;++i)
                     index[i] = nullptr;
@@ -119,35 +120,49 @@ namespace sjtu {
             }
         };
 
-    public://说实话感觉这里面有些东西不应该是public，写完再说
-        char *buffer = new char[IOnum];
-        BPT* BPlusTree = new BPT;
-        myStack indexstack;
-        Node_index *tmpindex = nullptr;//我想用栈！！！！！！！懒人不想自己写。没错。
-        Node_leaf *leaf = nullptr,*tmpleaf = nullptr;//不知道tmpleaf能否用上？？？
+#ifdef DEBUGGING
+        public:
+#else
+    private:
+#endif
+        ///下面这些部分到底谁该public呢？
         //IO members
-        FILE* ptr_file = nullptr;
-        char name[55] = {EMPTY_NAME_FILE};
-        bool isopen = false;
-
-        //IO func members
-        /*
-         * 关于cstdio中函数的使用说明：
-         *fopen：开文件时一定得左边放一个FILE指针。
-         *在每次fread之前一定得来个fseek,并且一定要seek到要读的一位之前。
-         *   例：SEEK_SET等价于0，read出来就是从1开始读。
-         *每次都读出定长，就可以避免出现：有一次比之前读的少，但是字符串长度未改变的尴尬情形。
+        char buffer[IOnum];
+        FILE* ptr_file;
+        char name[50];
+        bool isopen;
+        //Basic info of BPTree
+        BPT BPlusTree;
+        //以下是每一次修改树的操作需要用到的信息
+        /**不知道这里的有没有必要写在类里面？如果能写在函数里面是最好。
+         * 如果你准备删了他们的话，要同时删：
+         * 构造函数
+         */
+        myStack indexstack;
+        Node_index *tmpindex;//我想用栈！！并且自己写了一个，但是不清楚具体的用途怎样。
+        Node_leaf *leaf,*tmpleaf;//不知道tmpleaf能否用上？？？
+        ///不清楚是不是需要用到tmpkey?
+#ifdef DEBUGGING
+        public:
+#else
+    private:
+#endif
+        /**IO func members__reference
+         * 我能够考虑的到的特殊情况全部都throw了。
+         * alloc(需要开辟空间的大小):开辟一块新的空间（即文件尾位置的更改），将文件指针指向新空间并且准备写入。
+         * IO_init():打开/创立一个空的文件。并将BPT基本的初始化信息写入
+         * IO_destruct():关闭文件。
+         * IO_getblock(begin):从begin处读入4K块，返回值：1表示普通节点，2表示叶节点。
+         * IO_getnodeBPT():读取BPT基本信息。
+         * IO_write():两个重载，分别处理叶子节点和普通节点的写入。
+         * IO_write_nodeBPT():写入BPT中的基本信息。
          */
         //allocator,as well as modify endoffset_file.
-        inline PTR_FILE_CONT alloc(PTR_FILE_CONT need){
-            PTR_FILE_CONT tmp = BPlusTree->endoffset_file;
+        inline PTR_FILE_CONT alloc(PTR_FILE_CONT need = IOnum){
+            PTR_FILE_CONT tmp = BPlusTree.endoffset_file;
             fseek(ptr_file,0,SEEK_END);
-            BPlusTree->endoffset_file += need;
+            BPlusTree.endoffset_file += need;
             return tmp;
-        };
-        //这个应该理解为删除吧？？？
-        void dealloc(){
-
         };
         //仅供default构造函数使用。没有写复制构造相关的。
         void IO_init(){
@@ -161,15 +176,19 @@ namespace sjtu {
                 isopen = true;
             }
             alloc(sizeof(BPT));
-            int tmp = fwrite(BPlusTree, sizeof(BPT),1,ptr_file);
-            if(!tmp) throw "in IO_init:cannot write node BPT";
-            fflush(ptr_file);
+            IO_write_nodeBPT();
         };
-        //0表示空树，1表示普通节点，2表示叶节点
-        //<=>read(),用学长说过的读入优化
+        void IO_destruct(){
+            if(isopen) {
+                int tmp = fclose(ptr_file);
+                if(tmp == EOF) throw "in IO_destruct:the file cannot be closed.";
+                isopen = false;
+            }
+        }
+
         char IO_getblock(PTR_FILE_CONT beg){
-            if(BPlusTree->sumnum_data == 0)
-                return '0';
+            if(BPlusTree->sumnum_data == 0) throw "in IO_getblock:the tree is empty";
+
             fseek(ptr_file,beg,SEEK_SET);
             fread(buffer, sizeof(char),IOnum,ptr_file);
             if(*buffer == 'i'){
@@ -186,8 +205,13 @@ namespace sjtu {
                 throw "in IO_getblock:this block has been deleted...";
             throw "in IO_getblock:invalid buffer block!!!Oh NO!!!";
         }
+        inline void IO_getnodeBPT(){
+            fseek(ptr_file,0,SEEK_SET);
+            fread(buffer,sizeof(BPT),1,ptr_file);
+            memcpy(&BPlusTree,buffer,sizeof(BPT));
+        }
 
-        inline void IO_write(Node_leaf* tmpl){
+        void IO_write(Node_leaf* tmpl){
             if(tmpl == nullptr)throw "in IO_write_leaf:the leaf is null= =";
             if(!(tmpl->type == 'l'||tmpl->type == 'd'))
                 throw "in IO_write_leaf:invalid leafnode= =";
@@ -196,8 +220,9 @@ namespace sjtu {
             fseek(ptr_file,tmpl->myself,SEEK_SET);
             int tmp = fwrite(buffer, sizeof(char),IOnum,ptr_file);
             if(!tmp) throw "in IO_write_leaf:cannot fwrite";
+            fflush(ptr_file);
         };
-        inline void IO_write(Node_index* tmpi){
+        void IO_write(Node_index* tmpi){
             if(tmpi == nullptr)throw "in IO_write_index:the index is null= =";
             if(!(tmpi->type == 'i'||tmpi->type == 'd'))
                 throw "in IO_write_index:invalid indexnode= =";
@@ -206,13 +231,22 @@ namespace sjtu {
             fseek(ptr_file,tmpi->myself,SEEK_SET);
             int tmp = fwrite(buffer, sizeof(char),IOnum,ptr_file);
             if(!tmp) throw "in IO_write_index:cannot fwrite";
+            fflush(ptr_file);
         };
-
-        void IO_open(){};
-        void IO_close(){};
-        void IO_clear(){};
-
-    public:
+        inline void IO_write_nodeBPT(){
+            int tmp = fwrite(&BPlusTree, sizeof(BPT),1,ptr_file);
+            if(!tmp) throw "in IO_write_nodeBPT:cannot write node BPT";
+            fflush(ptr_file);
+        }
+#ifdef DEBUGGING
+        public:
+#else
+    private:
+#endif
+        /**insert func members__reference
+         *
+         *
+         */
         //和上面的getblock相对应。
         inline PTR_FILE_CONT seekblock_index(Node_index* index,Key findingkey){
             if(index == nullptr) throw "in seekblock_index:index not exist";
@@ -306,8 +340,10 @@ namespace sjtu {
             // And other methods in iterator, please fill by yourself.
         };
         // Default Constructor and Copy Constructor
-        BTree() {
-            // Todo Default
+        BTree():ptr_file(nullptr),isopen(false),tmpindex(nullptr),tmpleaf(nullptr),leaf(nullptr){
+            if(BPlusTree.endoffset_file != 0) throw "in BTree():BPT is not initialized.";
+            IO_init();
+            name = strcpy(name,EMPTY_NAME_FILE);
         }
         BTree(const BTree& other) {
             // Todo Copy
@@ -316,7 +352,10 @@ namespace sjtu {
             // Todo Assignment
         }
         ~BTree() {
-            // Todo Destructor
+            IO_destruct();
+            delete leaf;
+            delete tmpleaf;
+            delete tmpindex;
         }
         // Insert: Insert certain Key-Value into the database
         // Return a pair, the first of the pair is the iterator point to the new
